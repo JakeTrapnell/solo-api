54e7527490c590b297b17f47b55ff26d4faf99ed
JakeTrapnell <trapnelljake@gmail.com>
2019-02-07 10:52:54 +0000

    password and email added to user. extra recipes added to import

--- src/main/java/com/qa/persistence/domain/UsersTable.java
+++ src/main/java/com/qa/persistence/domain/UsersTable.java
@@ -14,9 +14,14 @@ public class UsersTable {
 	private Long id;
 	@Size(min = 2, max = 20)
 	private String name;
+	private String password;
+	private String email;
 	
-	public UsersTable(String name) {
+	
+	public UsersTable(String name, String password, String email) {
 		this.name = name;
+		this.password = password;
+		this.email = email;
 	}
 	
 	public UsersTable() {
@@ -29,6 +34,22 @@ public class UsersTable {
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	public Long getId() {
